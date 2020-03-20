0b063516be52536622afe3da9f5d764fb641aec4
MaxZvarych <56836603+MaxZvarych@users.noreply.github.com>
2020-03-20 10:11:42 +0200

    Added new Requests

--- src/main/java/ua/lviv/iot/spring/first/rest/controller/StudentsController.java
+++ src/main/java/ua/lviv/iot/spring/first/rest/controller/StudentsController.java
@@ -1,11 +1,19 @@
 package ua.lviv.iot.spring.first.rest.controller;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
-
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,23 +23,54 @@ import ua.lviv.iot.spring.first.rest.model.Student;
 @RequestMapping("/students")
 @RestController
 public class StudentsController {
+	private Map<Integer, Student> students = new HashMap();
+	private AtomicInteger idCounter = new AtomicInteger();
+
+	@GetMapping
+	public List<Student> getStudents() {
+		return new LinkedList<Student>(students.values());
+	}
 
 	@GetMapping(path = "/{id}")
 	public Student getStudent(@PathVariable("id") Integer studentId) {
 		System.out.println(studentId);
-		return new Student("pedro", "aldomovar");
+		return students.get(studentId);
 	}
 
 //    @RequestMapping(method = { RequestMethod.GET, RequestMethod.PUT }, path = "/smartest")
 //    public Student getSmartestStudent() {
 //        return new Student("tapas", "karantino");
 //    }
-	@PostMapping
-	//(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_XML_VALUE})
-	public Student createStudent(@RequestBody Student student) {
+	@PostMapping(produces = { MediaType.APPLICATION_JSON_VALUE })
+	public Student createStudent(final @RequestBody Student student) {
 		System.out.println(student);
-		student.setId(2002);
+		student.setId(idCounter.incrementAndGet());
+		students.put(student.getId(), student);
+
 		return student;
+	}
+
+	@DeleteMapping(path = "/{id}")
+	public ResponseEntity<Student> deleteStudent(@PathVariable("id") Integer studentId) {
+		HttpStatus status = students.remove(studentId) == null ? HttpStatus.NOT_FOUND : HttpStatus.OK;
+		return ResponseEntity.status(status).build();
+	}
+	
+	@PutMapping(path = "/{id}")
+	public ResponseEntity<Student> updateStudent(final @PathVariable("id") Integer studentId, final @RequestBody Student student)
+	{
+		student.setId(studentId);
+		HttpStatus status;
+		if(students.containsKey(studentId)) {
+			students.put(studentId, student);
+			status=HttpStatus.OK;
+			return ResponseEntity.status(status).build();
+		}
+		else {
+			status = HttpStatus.NOT_FOUND;
+			return ResponseEntity.status(status).build();
+		}
+			
 	}
 
 }
