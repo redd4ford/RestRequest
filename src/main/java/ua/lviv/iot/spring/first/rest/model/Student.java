934322a7c34c375b3524fb0612bae5b7021342c8
Viktoriia Yehorova <55955298+redd4ford@users.noreply.github.com>
2020-04-21 10:48:10 +0300

    Fixed hibernate mappings and cleaned the code

--- src/main/java/ua/lviv/iot/spring/first/rest/model/Student.java
+++ src/main/java/ua/lviv/iot/spring/first/rest/model/Student.java
@@ -1,86 +1,79 @@
 package ua.lviv.iot.spring.first.rest.model;
 
-import java.util.Set;
-
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.NamedNativeQuery;
-import javax.persistence.NamedQuery;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.Set;
+import javax.persistence.*;
 
 @Entity
-@NamedNativeQuery(name = "Student.findBestStudent", query="select * from student where id = 1")
+@NamedNativeQuery(name = "Student.findBestStudent", query = "select * from student where id = 1")
 public class Student {
 
-	private String firstName;
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Integer id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Integer id;
+  @Column
+  private String firstName;
+  @Column
+  private String lastName;
 
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "group_id")
-	@JsonIgnoreProperties("students")
-	private Group group;
+  @ManyToOne(fetch = FetchType.EAGER)
+  @JoinColumn(name = "group_id")
+  @JsonIgnoreProperties("students")
+  private Group group;
 
-	private String lastName;
-	
-	@ManyToMany(mappedBy = "students")
-	private Set<Subject> subjects;
+  @ManyToMany(fetch = FetchType.EAGER)
+  @JoinTable(name = "students_subjects",
+             joinColumns = {@JoinColumn(name = "student_id", nullable = false)},
+                           inverseJoinColumns = {@JoinColumn(name = "subject_id", nullable = true)})
+  @JsonIgnoreProperties("students")
+  private Set<Subject> subjects;
 
-	public Student() {
+  public Student() {
+  }
 
-	}
+  public Student(String firstName, String lastName) {
+    super();
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
 
-	public Student(String firstName, String lastName) {
-		super();
-		this.firstName = firstName;
-		this.lastName = lastName;
-	}
+  public Set<Subject> getSubjects() {
+    return subjects;
+  }
 
-	public Set<Subject> getSubjects() {
-		return subjects;
-	}
+  public void setSubjects(Set<Subject> subjects) {
+    this.subjects = subjects;
+  }
 
-	public void setSubjects(Set<Subject> subjects) {
-		this.subjects = subjects;
-	}
+  public Group getGroup() {
+    return group;
+  }
 
-	public Group getGroup() {
-		return group;
-	}
+  public void setGroup(Group group) {
+    this.group = group;
+  }
 
-	public void setGroup(Group group) {
-		this.group = group;
-	}
+  public Integer getId() {
+    return id;
+  }
 
-	public Integer getId() {
-		return id;
-	}
+  public void setId(Integer id) {
+    this.id = id;
+  }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+  public String getFirstName() {
+    return firstName;
+  }
 
-	public String getFirstName() {
-		return firstName;
-	}
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+  public String getLastName() {
+    return lastName;
+  }
 
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
 }
