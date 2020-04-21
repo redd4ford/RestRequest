b22d88bb8c74de4a34ae567860bebbc9929ed2fb
Viktoriia Yehorova <55955298+redd4ford@users.noreply.github.com>
2020-04-21 10:47:25 +0300

    Cleaned the code

--- src/main/java/ua/lviv/iot/spring/first/dataaccess/StudentRepository.java
+++ src/main/java/ua/lviv/iot/spring/first/dataaccess/StudentRepository.java
@@ -1,21 +1,17 @@
 package ua.lviv.iot.spring.first.dataaccess;
 
 import java.util.List;
-
-import javax.persistence.NamedQuery;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import ua.lviv.iot.spring.first.rest.model.Student;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
 
-	List<Student> findAllByFirstName(String firstName);
-	
-	List<Student> findAllByFirstNameAndLastName(String firstName,String lastName);
-	
-	Student findBestStudent();
+  List<Student> findAllByFirstName(String firstName);
+
+  List<Student> findAllByFirstNameAndLastName(String firstName, String lastName);
+
+  Student findBestStudent();
+
 }
